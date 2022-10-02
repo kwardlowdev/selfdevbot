42cdbe870f2cdc869193f3b8245a11ec2f7c1a39
Kyle Wardlow <111018078+kwardlowdev@users.noreply.github.com>
2022-10-02 15:17:41 -0400

    Added script file and began coding.

---
+++ selfdev.js
@@ -0,0 +1,6 @@
+const affirmationWords = ["capable", "beautiful", "worthy", "passionate", "loved", "gorgeous", "amazing"];
+const reminders = ["Take a break and do some chores, working too long on one thing is procrastinating on others. You are allowed to work on a healthy pace.", "Be kind to yourself. You are not forever bound to the things you dislike about yourself, and you can always make efforts to be better.", "Stop catastrophe-worrying, you are likely overthinking. Breathe, and push through.", "Mistakes are human, and so are you. Embrace them and keep trying to improve.", "Don't mistake procrastination for time management. Examine your reasons for putting something off to see if you're just avoiding it.", "People are not attacking you when they criticize or question you. Afford others the benefit of the doubt and assume benevolence."];
+const jokeStructures = [];
+const jokeNouns = [];
+const jokeAdjectives = [];
+const jokeVerbs = [];
