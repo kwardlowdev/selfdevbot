f80200e975f13ebd88c00365a644893c1ee1534a
Kyle Wardlow <111018078+kwardlowdev@users.noreply.github.com>
2022-10-22 12:08:02 -0400

    Added more to randomizer data sets.

--- selfdev.js
+++ selfdev.js
@@ -5,7 +5,7 @@ In future, these will be more efficiently organized within their own file/db
 */
 
 const affirmationWords = ["capable", "beautiful", "worthy", "passionate", "loved", "gorgeous", "amazing", "determined", "valuable", "unique", "valid", "human"];
-const reminders = ["Take a break and do some chores, working too long on one thing is procrastinating on others. You are allowed to work at a healthy pace.", "Be kind to yourself. You are not forever bound to the things you dislike about yourself today, and you can always make efforts to be better.", "Stop catastrophe-worrying, you are likely overthinking. Breathe, and push through.", "Mistakes are human, and so are you. Embrace them and keep trying to improve.", "Don't mistake procrastination for time management. Examine your reasons for putting something off to see if you're just avoiding it.", "People are not attacking you when they criticize or question you. Afford others the benefit of the doubt and assume benevolence.", "Not everything will be perfect from the beginning. Embrace the journey and love your ability to improve everything you make.", "Every error is a doorway to gaining knowledge.", "There is no hardship, trial, or failing that you are alone in experiencing, and there is always a path out that others have walked before."];
+const reminders = ["Take a break and do some chores, working too long on one thing is procrastinating on others. You are allowed to work at a healthy pace.", "Be kind to yourself. You are not forever bound to the things you dislike about yourself today, and you can always make efforts to be better.", "Stop catastrophe-worrying, you are likely overthinking. Breathe, and push through.", "Mistakes are human, and so are you. Embrace them and keep trying to improve.", "Don't mistake procrastination for time management. Examine your reasons for putting something off to see if you're just avoiding it.", "People are not attacking you when they criticize or question you. Afford others the benefit of the doubt and assume benevolence.", "Not everything will be perfect from the beginning. Embrace the journey and love your ability to improve everything you make.", "Every error is a doorway to gaining knowledge.", "There is no hardship, trial, or failing that you are alone in experiencing, and there is always a path out that others have walked before.", "Falling short of a goal is no reason to abandon the journey. Keep going!"];
 const noun = [{
     singular: "butt",
     plural: "butts"
@@ -37,6 +37,9 @@ const adj = [{
 }, {
     solo: "honest",
     an: "an honest"
+}, {
+    solo: "awesome",
+    an: "an awesome"
 }];
 const verb = ["kiss", "post on Insta", "collect", "organize", "change", "hug", "buy from Amazon", "eat"];
 const jokeAuthors = [{
@@ -57,9 +60,12 @@ const jokeAuthors = [{
 },{
     first: "Mark",
     last: "Twain"
+},{
+    first: "Wayne",
+    last: "Gretzky"
 }];
 
-const jokeTypes = ["You must be the #noun_singular you wish to #verb in the world.", "You miss 100% of the #noun_plural you don't #verb.", "The #noun_singular is #adj_an thing to #verb.", "I #verb, therefore I am.", "Life is ten percent #noun_singular and ninety percent #noun_singular."];
+const jokeTypes = ["You must be the #noun_singular you wish to #verb in the world.", "You miss 100% of the #noun_plural you don't #verb.", "The #noun_singular is #adj_an thing to #verb.", "I #verb, therefore I am.", "Life is ten percent #noun_singular and ninety percent #noun_singular.", "Don't judge each day by the #noun_singular you #verb but by the #noun_plural that you #verb."];
 
 /* Functions */
 
